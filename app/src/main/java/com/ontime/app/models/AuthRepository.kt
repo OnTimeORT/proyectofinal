fe7c5339e4d175bfa1a35f8ac011718662fb6706
Rami <rtouron@ignetworks.com>
2020-09-21 21:49:41 -0300

    nueva arquitectura mvvm para el proyecto

---
+++ app/src/main/java/com/ontime/app/models/AuthRepository.kt
@@ -0,0 +1,83 @@
+package com.ontime.app.models
+
+import android.app.Application
+import android.util.Log
+import android.widget.Toast
+import androidx.lifecycle.MutableLiveData
+import com.facebook.AccessToken
+import com.google.firebase.auth.FacebookAuthProvider
+import com.google.firebase.auth.FirebaseAuth
+import com.google.firebase.auth.FirebaseUser
+import com.google.firebase.auth.GoogleAuthProvider
+
+class AuthRepository(var application: Application) {
+    var userMutableLiveData: MutableLiveData<FirebaseUser> = MutableLiveData<FirebaseUser>()
+    var firebaseAuth: FirebaseAuth = FirebaseAuth.getInstance()
+
+    fun login(email: String, password: String) {
+        firebaseAuth.signInWithEmailAndPassword(email, password)
+            .addOnCompleteListener(application.mainExecutor) { task ->
+                if (task.isSuccessful) {
+                    Log.d("Task message", "signInWithEmail:success")
+                    val user: FirebaseUser? = firebaseAuth.currentUser
+                    userMutableLiveData.postValue(firebaseAuth.currentUser)
+                } else {
+                    Log.w("Task message", "signInWithEmail:failure", task.exception)
+                    Toast.makeText(
+                        application.applicationContext, "Authentication failed.",
+                        Toast.LENGTH_SHORT
+                    ).show()
+                }
+            }
+    }
+
+    fun register(email: String, password: String) {
+        firebaseAuth.createUserWithEmailAndPassword(email, password)
+            .addOnCompleteListener(application.mainExecutor) { task ->
+                if (task.isSuccessful) {
+                    userMutableLiveData.postValue(firebaseAuth.currentUser)
+                    Log.e("Task message", "Successful")
+                    Toast.makeText(
+                        application.applicationContext, "Register successfull",
+                        Toast.LENGTH_SHORT
+                    ).show()
+                } else {
+                    Log.e("Task message", "Error", task.exception)
+                    Toast.makeText(
+                        application.applicationContext, "Register failed.",
+                        Toast.LENGTH_SHORT
+                    ).show()
+                }
+            }
+    }
+
+    fun firebaseAuthWithGoogle(idToken: String) {
+        val credential = GoogleAuthProvider.getCredential(idToken, null)
+        firebaseAuth.signInWithCredential(credential)
+            .addOnCompleteListener { task ->
+                if (task.isSuccessful) {
+                    Log.d("Task message", "signInWithGoogle:success")
+                    userMutableLiveData.postValue(firebaseAuth.currentUser)
+                } else {
+                    Log.w("Task message", "signInWithGoogle:failure", task.exception)
+                }
+            }
+    }
+
+    fun firebaseAuthWithFacebook(token: AccessToken) {
+        val credential = FacebookAuthProvider.getCredential(token.token)
+        firebaseAuth.signInWithCredential(credential)
+            .addOnCompleteListener { task ->
+                if (task.isSuccessful) {
+                    Log.d("Task message", "signInWithFacebook:success")
+                    userMutableLiveData.postValue(firebaseAuth.currentUser)
+                } else {
+                    Log.w("Task message", "signInWithFacebook:failure", task.exception)
+                    Toast.makeText(
+                        application.applicationContext, "Authentication failed.",
+                        Toast.LENGTH_SHORT
+                    ).show()
+                }
+            }
+    }
+}
