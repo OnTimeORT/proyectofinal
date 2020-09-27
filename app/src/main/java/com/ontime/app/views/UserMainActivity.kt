2cd3dc379ef4807d31e0a4009c491bbcd12140ac
Rodrigo Iglesias <rniglesias@gmail.com>
2020-09-27 01:42:10 -0300

    Agregado un logout más elegante

--- app/src/main/java/com/ontime/app/views/UserMainActivity.kt
+++ app/src/main/java/com/ontime/app/views/UserMainActivity.kt
@@ -1,17 +1,20 @@
 package com.ontime.app.views
 
-import androidx.appcompat.app.AppCompatActivity
+import android.content.Intent
 import android.os.Bundle
 import android.view.MenuItem
 import android.view.View
-import android.widget.Toast
 import androidx.appcompat.app.ActionBarDrawerToggle
+import androidx.appcompat.app.AppCompatActivity
 import androidx.appcompat.widget.Toolbar
 import androidx.core.view.GravityCompat
 import androidx.drawerlayout.widget.DrawerLayout
 import androidx.fragment.app.Fragment
 import com.google.android.material.navigation.NavigationView
+import com.google.firebase.auth.FirebaseAuth
+import com.ontime.app.MainActivity
 import com.ontime.app.R
+
 
 class UserMainActivity : AppCompatActivity(), NavigationView.OnNavigationItemSelectedListener {
 
@@ -89,7 +92,14 @@ class UserMainActivity : AppCompatActivity(), NavigationView.OnNavigationItemSel
             // al main activity
         }
 
-        fragmentManager.beginTransaction().replace(R.id.fragment_container, fragment!!).commit()
+        if ( fragment == null) {
+            // Desloguear y volver al login
+            FirebaseAuth.getInstance().signOut()
+            startActivity(Intent(this, MainActivity::class.java))
+
+        } else {
+            fragmentManager.beginTransaction().replace(R.id.fragment_container, fragment!!).commit()
+        }
         val drawer =
             findViewById<View>(R.id.drawer_layout) as DrawerLayout
         drawer.closeDrawer(GravityCompat.START)
